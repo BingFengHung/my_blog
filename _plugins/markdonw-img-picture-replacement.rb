2abe65e3d4beb5afe814fb71d6ddb7205e456dc0
BingFengHung <joe94008@gmail.com>
2022-06-18 14:50:37 +0800

    <fix>: GitHub 圖片路徑測試

--- _plugins/markdonw-img-picture-replacement.rb
+++ _plugins/markdonw-img-picture-replacement.rb
@@ -1,14 +1,16 @@
 
 # Description: Jekyll plugin to replace Markdown image syntax with {% picture %} tag for crafting responsive images
 # place in /_plugins/
+# newContent = document.content.tr('../assets', '{{site.url}}/assets')
+# newContent = document.content.gsub(/..\/assets/, '{{site.url}}/assets')
+# newContent = document.content.gsub(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '{% picture default \2 --alt \1  %}')
+# document.content = newContent
 
 Jekyll::Hooks.register :documents, :pre_render do |document, payload|
   docExt = document.extname.tr('.', '')
   # only process if we deal with a markdown file
   if payload['site']['markdown_ext'].include? docExt
-		#newContent = document.content.tr('../assets', '{{site.url}}/assets')
 		newContent = document.content.gsub(/..\/assets/, '{{site.url}}/assets')
-    # newContent = document.content.gsub(/!\[(.*)\]\(([^\)]+)\)(?:{:([^}]+)})*/, '{% picture default \2 --alt \1  %}')
     document.content = newContent
   end
 end
