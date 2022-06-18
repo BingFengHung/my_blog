8f742cd3e72da8f3b6d6357f3fd44122feb9a5bc
BingFengHung <joe94008@gmail.com>
2022-06-18 13:31:14 +0800

    <fix>: GitHub 圖片路徑測試

--- _plugins/markdonw-img-picture-replacement.rb
+++ _plugins/markdonw-img-picture-replacement.rb
@@ -6,7 +6,8 @@ Jekyll::Hooks.register :posts, :pre_render do |post, payload|
   docExt = post.extname.tr('.', '')
   # only process if we deal with a markdown file
   if payload['site']['markdown_ext'].include? docExt
-    newContent = post.content.gsub(/\!\[(.+)\]\((.+)\)/, '{% picture default \2 alt="\1" %}')
+    # newContent = post.content.gsub(/\!\[(.+)\]\((.+)\)/, '{% picture default \2 alt="\1" %}')
+    newContent = post.content.gsub(/\!\[(.+)\]\((.+)\)/, '{% picture %}')
     post.content = newContent
   end
 end
